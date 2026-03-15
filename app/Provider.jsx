62b563133b1193ff9f4829ad92b9cdf9b96565d8
swapnilraut-teck <swapnilrautrs152@gmail.com>
2026-03-15 15:50:09 +0530

    feat(ui): add theme provider and toggle
    - Integrate next-themes for dark/light mode support
    - Add theme toggle buttons to home page

---
+++ app/Provider.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { ThemeProvider as NextThemesProvider } from "next-themes";
+const Provider = ({ children, ...props }) => {
+  return (
+    <NextThemesProvider
+      attribute="class"
+      defaultTheme="dark"
+      enableSystem
+      disableTransitionOnChange
+      {...props}
+    >
+      {children}
+    </NextThemesProvider>
+  );
+};
+
+export default Provider
--- app/page.js
+++ app/page.js
@@ -1,12 +1,21 @@
+'use client'
 import { Button } from "@/components/ui/button";
+import { useTheme } from "next-themes";
 import Image from "next/image";
 
 export default function Home() {
+  const {setTheme}  = useTheme()
   return (
     <div>
       <h1>this swapnil ai fusion lab</h1>
       <Button >
         click
+      </Button>
+      <Button onClick={() => setTheme('light')}>
+        light mode
+      </Button>
+      <Button onClick={() => setTheme('dark')}>
+        dark mode
       </Button>
     </div>
   );
