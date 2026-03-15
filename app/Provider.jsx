7530994f67e707b052e200206ba9f2f24409daa2
swapnilraut-teck <swapnilrautrs152@gmail.com>
2026-03-15 17:01:12 +0530

    style(ui): remove trailing whitespace in

--- app/Provider.jsx
+++ app/Provider.jsx
@@ -8,7 +8,7 @@ const Provider = ({ children, ...props }) => {
       enableSystem
       disableTransitionOnChange
       {...props}
-    >
+    > 
       {children}
     </NextThemesProvider>
   );
